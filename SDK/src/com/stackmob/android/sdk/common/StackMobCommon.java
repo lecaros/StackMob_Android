673d2ccabd0dc43b36178500fe4517c7a00b268c
Aaron Schlesinger <arschles@gmail.com>
2011-10-27 15:45:04 -0700

    ability to set API and push API URL mostly used for internal testing right now

--- SDK/src/com/stackmob/android/sdk/common/StackMobCommon.java
+++ SDK/src/com/stackmob/android/sdk/common/StackMobCommon.java
@@ -17,7 +17,9 @@
 package com.stackmob.android.sdk.common;
 
 import com.stackmob.android.sdk.callback.*;
+import com.stackmob.sdk.callback.StackMobRedirectedCallback;
 import com.stackmob.sdk.api.StackMob;
+import java.util.Map;
 
 public class StackMobCommon {
 	public static String API_KEY = "YOUR_API_KEY_HERE";
@@ -25,20 +27,29 @@ public class StackMobCommon {
 	public static String USER_OBJECT_NAME = "YOUR_USER_OBJECT_NAME_HERE";
 	public static Integer API_VERSION = 0;
 	
+	public static String API_URL_FORMAT = "api.mob1.stackmob.com";
+	public static String PUSH_API_URL_FORMAT = "push.mob1.stackmob.com";
+	
 	public static String TWITTER_CONSUMER_KEY = "YOUR_TWITTER_CONSUMER_KEY_HERE";
 	public static String TWITTER_CONSUMER_SECRET = "YOUR_TWITTER_CONSUMER_SECRET_HERE";
 	
 	public static String FACEBOOK_APP_ID = "YOUR_FACEBOOK_APP_ID_HERE";
 	
+	public static StackMobTwitterCallback TwitterCallback = null;
+	public static StackMobFacebookCallback FacebookCallback = null;
+	 
 	private static StackMob stackmob;
+	
+	private static StackMobRedirectedCallback redirectedCallback = new StackMobRedirectedCallback() {
+		@Override public void redirected(String originalURL, Map<String, String> redirectHeaders, String redirectBody, String newURL) {
+			//do nothing for now
+		}
+	};
 	
 	public static StackMob getStackMobInstance() {
 		if(stackmob == null) {
-			stackmob = new StackMob(API_KEY, API_SECRET, USER_OBJECT_NAME, API_VERSION);
+			stackmob = new StackMob(API_KEY, API_SECRET, USER_OBJECT_NAME, API_VERSION, API_URL_FORMAT, PUSH_API_URL_FORMAT, redirectedCallback);
 		}
 		return stackmob;
 	}
-	
-	public static StackMobTwitterCallback TwitterCallback = null;
-	public static StackMobFacebookCallback FacebookCallback = null;
 }
